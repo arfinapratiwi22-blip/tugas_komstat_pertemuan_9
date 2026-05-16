827fcf02785ae4674bd6f5f6a34c587e4394e134
arfina <arfinapratiwi22@gmail.com>
2026-05-16 20:18:29 +0700

    Initial commit: Membuat dataset curah hujan harian

---
+++ data curah hujan.R
@@ -0,0 +1,6 @@
+# 1. Membuat dataset curah hujan harian selama 5 hari (dalam mm)
+curah_hujan <- c(12.5, 24.0, 5.8, 30.2, 15.1)
+
+# 2. Menghitung ringkasan statistik deskriptif data (Min, Kuartil, Median, Max)
+ringkasan_cuaca <- summary(curah_hujan)
+print(ringkasan_cuaca)
