f5da6efe38dfc778f1e1d6f9dfc132e867a4161f
arfina <arfinapratiwi22@gmail.com>
2026-05-16 20:24:50 +0700

    tidak sengaja merusak data

--- data curah hujan.R
+++ data curah hujan.R
@@ -4,3 +4,7 @@ curah_hujan <- c(12.5, 24.0, 5.8, 30.2, 15.1)
 # 2. Menghitung ringkasan statistik deskriptif data (Min, Kuartil, Median, Max)
 ringkasan_cuaca <- summary(curah_hujan)
 print(ringkasan_cuaca)
+
+# Kesalahan: Tidak sengaja merusak data
+curah_hujan <- NULL
+total_curah_hujan <- sum(curah_hujan)
